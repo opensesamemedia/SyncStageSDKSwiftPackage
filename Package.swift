f668166e247a3462718d5a0cf69b572ab6ad5fe1
bilal.mahfouz <b.mahfouz.86@gmail.com>
2022-06-27 14:46:36 +0300

    update sdk to 0.0.5

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     ],
     dependencies: [],
     targets: [
-        .binaryTarget(name: "SyncStageSDK", url: "https://syncstage.s3.amazonaws.com/sdk/SyncStageSDK_0.0.4.xcframework.zip", checksum: "489d0aa2b26f9ee786744b1b08524930f0c692514ced8d85a123008b16a7238f")
+        .binaryTarget(name: "SyncStageSDK", url: "https://syncstage.s3.amazonaws.com/sdk/SyncStageSDK_0.0.5.xcframework.zip", checksum: "9c1da1b1e7becc7ee926299a7eb8fad0637bc9240441ad0f3f2b0688a9bff0e6")
     ],
     swiftLanguageVersions: [.v5]
 )
