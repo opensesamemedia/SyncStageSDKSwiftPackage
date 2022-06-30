503772b2449d04e57063f80bb98ac20f4a799bee
Mateusz <mateusz28@gmail.com>
2022-06-30 13:51:35 +0200

    Release 0.0.6

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     ],
     dependencies: [],
     targets: [
-        .binaryTarget(name: "SyncStageSDK", url: "https://syncstage.s3.amazonaws.com/sdk/SyncStageSDK_0.0.5.xcframework.zip", checksum: "9c1da1b1e7becc7ee926299a7eb8fad0637bc9240441ad0f3f2b0688a9bff0e6")
+        .binaryTarget(name: "SyncStageSDK", url: "https://syncstage.s3.amazonaws.com/sdk/SyncStageSDK_0.0.6.xcframework.zip", checksum: "57fe63a79c11128054d75b161f404569b55f3e4d5537bc23b8f8d7d191a6d2e2")
     ],
     swiftLanguageVersions: [.v5]
 )
