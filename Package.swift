131aa527a0a9a02153a425f0173efa4d0e688f40
Mateusz <mateusz28@gmail.com>
2022-07-06 15:41:35 +0200

    Fixed 0.0.7 checksum

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     ],
     dependencies: [],
     targets: [
-        .binaryTarget(name: "SyncStageSDK", url: "https://syncstage.s3.amazonaws.com/sdk/SyncStageSDK_0.0.7.xcframework.zip", checksum: "43c41289a1896387eb8485186391a3c0")
+        .binaryTarget(name: "SyncStageSDK", url: "https://syncstage.s3.amazonaws.com/sdk/SyncStageSDK_0.0.7.xcframework.zip", checksum: "2b61af0a0dd02af0b874978c26bf0f10679215d18e8613a79c42443e42844256")
     ],
     swiftLanguageVersions: [.v5]
 )
