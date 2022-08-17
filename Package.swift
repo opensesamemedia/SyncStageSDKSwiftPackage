994c5acff90779af6c41083c849afd808b3bbef4
bilal.mahfouz <b.mahfouz.86@gmail.com>
2022-08-17 17:32:23 +0300

    release new sdk version 0.0.13

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     ],
     dependencies: [],
     targets: [
-        .binaryTarget(name: "SyncStageSDK", url: "https://syncstage.s3.amazonaws.com/sdk/SyncStageSDK_0.0.12.xcframework.zip", checksum: "dd62ef82767552b6c93bc43279a90369837dc0e5ff68e63da8183f762b7bf7d7")
+        .binaryTarget(name: "SyncStageSDK", url: "https://syncstage.s3.amazonaws.com/sdk/SyncStageSDK_0.0.13.xcframework.zip", checksum: "2fe35ec434aa782b1f650134fcdde4d1338f3dabc38c6f27cb3c1606e7a539d6")
     ],
     swiftLanguageVersions: [.v5]
 )
